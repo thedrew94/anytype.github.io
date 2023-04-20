01e8016a9d4716b3df17b664c8257bed23a446a8
Tornyai Laurentiu Andrei <121413180+thedrew94@users.noreply.github.com>
2023-04-20 12:30:03 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,8 @@
-"use strict";
-
-const carousel = document.querySelector(".carousel");
-
-let isDragStart = false,
-  prevPageX,
-  prevScrollLeft;
-
-const dragStart = function (event) {
-  isDragStart = true;
-  prevPageX = event.pageX;
-  prevScrollLeft = carousel.scrollLeft;
-};
-
-const dragStop = function (event) {
-  isDragStart = false;
-};
-
-const dragging = function (event) {
-  if (!isDragStart) return;
-  event.preventDefault();
-  let positionDiff = event.pageX - prevPageX;
-  carousel.scrollLeft = prevScrollLeft - positionDiff;
-  console.log(event.pageX);
-};
-
-carousel.addEventListener("mousedown", dragStart);
-carousel.addEventListener("mouseup", dragStop);
-carousel.addEventListener("mouseleave", dragStop);
-carousel.addEventListener("mousemove", dragging);
-
-carousel.addEventListener("touchstart", dragStart);
-carousel.addEventListener("touchend", dragStop);
-carousel.addEventListener("touchcancel", dragStop);
-carousel.addEventListener("touchmove", dragging);
+window.addEventListener("resize", () => {
+  if (window.innerWidth <= 70 * 16) {
+    // 70rem * 16px per rem
+    const webpage = document.querySelector(".webpage");
+    document.body.removeChild(webpage);
+  }
+});
 
