680d638a3c98c6ab29172addd67565e315d3331f
Tornyai Laurentiu Andrei <121413180+thedrew94@users.noreply.github.com>
2023-05-08 15:08:47 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -44,31 +44,31 @@ square.addEventListener("click", function () {
 const modalWindow = document.querySelector(".modal-window");
 const closeBtn = document.querySelector(".close-btn");
 const playBtn = document.querySelector(".play-button");
+const bodyElement = document.querySelector("body");
+const iframe = document.querySelector("#video-player");
 
 closeBtn.addEventListener("click", () => {
   modalWindow.classList.toggle("opened-style");
   modalWindow.classList.toggle("closed-style");
+  bodyElement.classList.toggle("overflowClass");
+  iframe.src = ""; // set the src attribute to an empty string to stop the video
 });
 
 playBtn.addEventListener("click", () => {
   modalWindow.classList.toggle("closed-style");
   modalWindow.classList.toggle("opened-style");
+  bodyElement.classList.toggle("overflowClass");
+  iframe.src = "https://www.youtube.com/embed/kCLmXPSI5_w?autoplay=1";
 });
 
-var player;
-function onYouTubePlayerAPIReady() {
-  player = new YT.Player("player", {
-    height: "720",
-    width: "2120",
-    videoId: "kCLmXPSI5_w",
-    playerVars: {
-      autoplay: 0,
-      controls: 1,
-    },
-  });
-}
-
-window.onYouTubePlayerAPIReady = onYouTubePlayerAPIReady;
+modalWindow.addEventListener("click", (event) => {
+  if (event.target === modalWindow) {
+    modalWindow.classList.toggle("opened-style");
+    modalWindow.classList.toggle("closed-style");
+    bodyElement.classList.toggle("overflowClass");
+    iframe.src = "";
+  }
+});
 
 // ////////////////////////////////////////////
 // REDIRECTING USER TO DOWNLOAD APP CODE
@@ -82,12 +82,21 @@ downloadBtn.forEach((btn) => {
     const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
     if (!isMobile) {
+      // If user is on pc open the App Store link
       window.open(
         "https://apps.apple.com/jp/app/anytype-%E6%97%A5%E6%9C%AC%E8%AA%9E%E6%96%87%E5%AD%97%E5%85%A5%E5%8A%9B-%E7%9D%80%E3%81%9B%E6%9B%BF%E3%81%88%E3%82%AD%E3%83%BC%E3%83%9C%E3%83%BC%E3%83%89/id1078897849",
         "_blank"
       );
     } else {
-      window.location.href = "itms-apps://itunes.apple.com/app/id1078897849";
+      // If user is on Android device open Google Play Store link
+      if (/Android/i.test(navigator.userAgent)) {
+        window.location.href =
+          "https://play.google.com/store/apps/details?id=com.omega.anytype";
+      }
+      // If user is on iOS open App Store link
+      else {
+        window.location.href = "itms-apps://itunes.apple.com/app/id1078897849";
+      }
     }
   });
 });
@@ -100,12 +109,25 @@ const menuVoices = document.querySelectorAll(".menu-voice");
 menuVoices.forEach((menuVoice) => {
   menuVoice.addEventListener("click", () => {
     const targetId = menuVoice.getAttribute("id");
-    console.log(targetId);
     const targetElement = document.getElementById(`target${targetId}`);
-    console.log(targetElement);
     targetElement.scrollIntoView({ behavior: "smooth" });
   });
 });
 
-// ////////////////////////////////////////////
+// SCROLL INTO VIEW FOR KEYBOARD SETTINGS
+const keyboardLink = document.querySelectorAll(".keyboard-link");
+const greenPage = document.querySelector(".green-page-element");
 
+keyboardLink.forEach((keyboard) => {
+  keyboard.addEventListener("click", () => {
+    greenPage.scrollIntoView({
+      behavior: "smooth",
+    });
+    modalWindow.classList.toggle("closed-style");
+    modalWindow.classList.toggle("opened-style");
+    bodyElement.classList.toggle("overflowClass");
+    iframe.src = "https://www.youtube.com/embed/kCLmXPSI5_w?autoplay=1";
+  });
+});
+
+// ////////////////////////////////////////////
